3046aa3a6a15e46c82d33b95ed3b02b31f262c9c
Denis Savchenko <denis.savchenko98@yandex.ru>
2023-08-23 16:05:00 +0300

    Repository layer test created

--- src/main/java/org/jacp/repository/QuestionRepository.java
+++ src/main/java/org/jacp/repository/QuestionRepository.java
@@ -4,11 +4,14 @@ import org.jacp.entity.QuestionEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 /**
  * @author saffchen created on 31.07.2023
  */
 @Transactional(readOnly = true)
 @Repository
 public interface QuestionRepository extends BaseRepository<QuestionEntity> {
-
+    @Override
+    Optional<QuestionEntity> findById(Long id);
 }
--- src/test/java/org/jacp/repository/QuestionRepositoryTest.java
+++ src/test/java/org/jacp/repository/QuestionRepositoryTest.java
@@ -1,50 +1,32 @@
 package org.jacp.repository;
 
 import org.jacp.entity.QuestionEntity;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import java.util.Optional;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * @author saffchen created on 11.08.2023
+ * @author saffchen created on 22.08.2023
  */
-@ExtendWith(MockitoExtension.class)
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class QuestionRepositoryTest {
-    @Mock
-    private QuestionRepository questionRepository;
+    @Autowired
+    QuestionRepository questionRepository;
 
     @Test
-    public void testFindById() {
-        QuestionEntity questionEntity = new QuestionEntity();
-        Long questionId = 1L;
-        String problem = "TestProblem";
-        String difficult = "TestDifficult";
-        String description = "TestDescription";
-        String imports = "TestImports";
-        String body = "TestBody";
-        String test = "TestTest";
-        questionEntity.setId(questionId);
-        questionEntity.setProblem(problem);
-        questionEntity.setDifficult(difficult);
-        questionEntity.setDescription(description);
-        questionEntity.setImports(imports);
-        questionEntity.setBody(body);
-        questionEntity.setTest(test);
-
-        Mockito.when(questionRepository.findById(1L)).thenReturn(Optional.of(questionEntity));
-
-        Optional<QuestionEntity> result = questionRepository.findById(1L);
-
-        assertThat(result.isPresent()).isTrue();
-        assertThat(result.get().getId()).isEqualTo(1L);
-        assertThat(result.get().getProblem()).isEqualTo("TestProblem");
-
-        Mockito.verify(questionRepository).findById(1L);
+    void testFindById() {
+        Long id = 1L;
+        Optional<QuestionEntity> optionalQuestion = questionRepository.findById(id);
+        assertTrue(optionalQuestion.isPresent());
+        QuestionEntity question = optionalQuestion.get();
+        assertEquals(id, question.getId());
+        assertEquals("Grasshopper", question.getProblem());
     }
-
 }
