b572b36882b053c1aa181f2ebc151b62a83f5781
Denis Savchenko <denis.savchenko98@yandex.ru>
2023-08-24 21:38:05 +0300

    Created abstract integration test class

---
+++ src/test/java/org/jacp/IntegrationTestBase.java
@@ -0,0 +1,22 @@
+package org.jacp;
+
+import org.jacp.initializer.Postgres;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author saffchen created on 24.08.2023
+ */
+@ActiveProfiles("test")
+@Transactional
+@ContextConfiguration(initializers = Postgres.Initializer.class)
+@SpringBootTest
+public abstract class IntegrationTestBase {
+    @BeforeAll
+    public static void init(){
+        Postgres.container.start();
+    }
+}
