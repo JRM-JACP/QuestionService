4b95b8d270dc271e94335e3f9c74d6f08d836491
Denis Savchenko <denis.savchenko98@yandex.ru>
2023-09-10 11:39:06 +0300

    Create Import Mapper Test

--- src/test/java/org/jacp/mapper/ImportMapperTest.java
+++ src/test/java/org/jacp/mapper/ImportMapperTest.java
@@ -5,9 +5,12 @@ import org.jacp.entity.ImportEntity;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,16 +20,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(MockitoExtension.class)
 public class ImportMapperTest {
 
-    @Mock
-    private ImportEntity entity;
-
     @InjectMocks
     private ImportMapperImpl importMapper;
 
+
     @Test
     void importToImportDto() {
-        Mockito.when(entity.getImports()).thenReturn("import lombok.AllArgsConstructor;");
-        ImportDto importDto = importMapper.importToImportDto(entity);
-        assertEquals("import lombok.AllArgsConstructor;", importDto.getImports());
+        List<ImportEntity> importEntity = Arrays.asList(
+                new ImportEntity(1L, "import 1"),
+                new ImportEntity(2L, "import 2"),
+                new ImportEntity(3L, "import 3")
+        );
+
+        List<ImportDto> expectedImportDto = Arrays.asList(
+                new ImportDto(List.of("import 1")),
+                new ImportDto(List.of("import 2")),
+                new ImportDto(List.of("import 3"))
+        );
+
+        String expectedImports = expectedImportDto.stream()
+                .map(ImportDto::getImports)
+                .map(Objects::toString)
+                .collect(Collectors.joining());
+
+        List<ImportDto> actualImportDto = importMapper.toImportDto(importEntity);
+
+        String actualImports = actualImportDto.stream()
+                .map(ImportDto::getImports)
+                .map(Objects::toString)
+                .collect(Collectors.joining());
+
+        assertEquals(expectedImports, actualImports);
     }
 }
