87e9697641a982af9e76d072553bccd96aa426bc
Denis Savchenko <denis.savchenko98@yandex.ru>
2023-09-10 22:17:01 +0300

    Create Import Service Test

--- src/test/java/org/jacp/service/ImportServiceTest.java
+++ src/test/java/org/jacp/service/ImportServiceTest.java
@@ -1,27 +1,25 @@
 package org.jacp.service;
 
 import org.jacp.entity.ImportEntity;
-import org.jacp.repository.ImportRepository;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * @author saffchen created on 07.09.2023
  */
 @ExtendWith(MockitoExtension.class)
 public class ImportServiceTest {
-
-    @Mock
-    private ImportRepository importRepository;
 
     @InjectMocks
     private ImportService importService;
@@ -31,22 +29,39 @@ public class ImportServiceTest {
         MockitoAnnotations.openMocks(this);
     }
 
-    static ImportEntity importEntity = new ImportEntity();
-
-    static Long id = 1L;
-
-    static String imports = "import lombok.AllArgsConstructor;";
-
-    @BeforeAll
-    static void prepareData() {
-        importEntity.setId(id);
-        importEntity.setImports(imports);
-    }
+    static Long id1 = 1L;
+    static Long id2 = 2L;
+    static Long id3 = 3L;
+    static String imports1 = "import 1";
+    static String imports2 = "import 2";
+    static String imports3 = "import 3";
 
     @Test
-    void getAllTest() {
-        Mockito.when(importRepository.getAll()).thenReturn(importEntity);
-        ImportEntity result = importService.getAll();
-        assertEquals(importEntity.getImports(), result.getImports());
+    public void testGetAll() {
+        List<ImportEntity> importEntity = Arrays.asList(
+                new ImportEntity(id1, imports1),
+                new ImportEntity(id2, imports2),
+                new ImportEntity(id3, imports3)
+        );
+
+        List<ImportEntity> expectedEntity = Arrays.asList(
+                new ImportEntity(id1, imports1),
+                new ImportEntity(id2, imports2),
+                new ImportEntity(id3, imports3)
+        );
+
+        Mockito.when(importService.getAll()).thenReturn(importEntity);
+        List<ImportEntity> result = importService.getAll();
+        String resultString = result.stream()
+                .map(ImportEntity::getImports)
+                .map(Objects::toString)
+                .collect(Collectors.joining());
+
+        String expectedString = expectedEntity.stream()
+                .map(ImportEntity::getImports)
+                .map(Objects::toString)
+                .collect(Collectors.joining());
+
+        Assertions.assertEquals(expectedString, resultString);
     }
 }
