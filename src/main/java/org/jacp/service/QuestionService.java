9a757adb7cd1db40edcdec95cd9f8d57c6a42fd5
Denis Savchenko <denis.savchenko98@yandex.ru>
2024-01-03 16:18:33 +0300

    Added new method service for getting tasks based on filters

--- src/main/java/org/jacp/service/QuestionService.java
+++ src/main/java/org/jacp/service/QuestionService.java
@@ -1,10 +1,15 @@
 package org.jacp.service;
 
 import lombok.AllArgsConstructor;
+import org.jacp.dto.SearchDto;
 import org.jacp.entity.QuestionEntity;
 import org.jacp.error.NoEntityException;
 import org.jacp.repository.QuestionRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author saffchen created on 01.08.2023
@@ -18,5 +23,12 @@ public class QuestionService {
     public QuestionEntity get(Long id) throws NoEntityException {
         return questionRepository.findById(id)
                 .orElseThrow(() -> new NoEntityException("Question with id " + id + " is not found"));
+    }
+
+    public List<QuestionEntity> getFilter(SearchDto searchDto) {
+        Pageable pageable = PageRequest.of(0, searchDto.getLimitTasks());
+        return questionRepository.findByFilter(pageable, searchDto.getDifficulty(), searchDto.getTagsList())
+                .orElseThrow(() -> new NoEntityException("Question with difficulty " + searchDto.getDifficulty() +
+                        " or tags " + searchDto.getTagsList() + " is not found"));
     }
 }
