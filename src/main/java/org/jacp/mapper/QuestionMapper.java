048dcd6e662eb3b4f33081263cba54bd1096fff0
Denis Savchenko <denis.savchenko98@yandex.ru>
2024-01-03 16:20:26 +0300

    Added new method mapper for list QuestionEntity

--- src/main/java/org/jacp/mapper/QuestionMapper.java
+++ src/main/java/org/jacp/mapper/QuestionMapper.java
@@ -5,10 +5,14 @@ import org.jacp.entity.QuestionEntity;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 /**
  * @author saffchen created on 02.08.2023
  */
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.FIELD)
 public interface QuestionMapper {
     QuestionDto toQuestionDto(QuestionEntity questionEntity);
+
+    List<QuestionDto> toListQuestionDto(List<QuestionEntity> questionEntities);
 }
