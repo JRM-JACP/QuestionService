e5b2858e8a7f2a068398982930ff16e09ecf36ee
Denis Savchenko <denis.savchenko98@yandex.ru>
2024-01-03 16:18:23 +0300

    Added new method repository for getting tasks based on filters

--- src/main/java/org/jacp/repository/QuestionRepository.java
+++ src/main/java/org/jacp/repository/QuestionRepository.java
@@ -1,9 +1,14 @@
 package org.jacp.repository;
 
 import org.jacp.entity.QuestionEntity;
+import org.jacp.enums.Difficulty;
+import org.jacp.enums.Tags;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -14,4 +19,8 @@ import java.util.Optional;
 public interface QuestionRepository extends BaseRepository<QuestionEntity> {
     @Override
     Optional<QuestionEntity> findById(Long id);
+
+    @Query("select qe from QuestionEntity qe where qe.difficulty=:difficulty or :difficulty is null " +
+            "and :tags member of qe.tags or :tags is null order by random()")
+    Optional<List<QuestionEntity>> findByFilter(Pageable pageable, Difficulty difficulty, Tags tags);
 }
