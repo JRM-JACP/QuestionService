66c61ca3bca632f80b9f34c1f88bd9f062a56505
Denis Savchenko <denis.savchenko98@yandex.ru>
2024-01-19 13:51:41 +0300

    Method moved to another class

--- src/main/java/org/jacp/repository/QuestionRepository.java
+++ src/main/java/org/jacp/repository/QuestionRepository.java
@@ -1,14 +1,9 @@
 package org.jacp.repository;
 
 import org.jacp.entity.QuestionEntity;
-import org.jacp.enums.Difficulty;
-import org.jacp.enums.Tags;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -19,8 +14,4 @@ import java.util.Optional;
 public interface QuestionRepository extends BaseRepository<QuestionEntity> {
     @Override
     Optional<QuestionEntity> findById(Long id);
-
-    @Query("select qe from QuestionEntity qe where qe.difficulty=:difficulty or :difficulty is null " +
-            "and :tags member of qe.tags or :tags is null order by random()")
-    Optional<List<QuestionEntity>> findByFilter(Pageable pageable, Difficulty difficulty, Tags tags);
 }
